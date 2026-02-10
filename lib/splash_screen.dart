b6062447a6cb382dc23a6d875b6b31fea53a0b81
anshifahz <anshfanshifahz@gmail.com>
2026-02-10 16:35:02 +0530

    fourth commit

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,17 +10,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // Navigate to Login Screen after 3 seconds
-    Timer(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    });
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
